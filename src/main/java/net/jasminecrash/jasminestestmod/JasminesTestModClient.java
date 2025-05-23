216d77895d0c0cd83ce8c7ae4a1f4c280128b89a
JasmineCrash <jazzminecrash@gmail.com>
2025-05-23 13:21:43 -0700

    First commit! Hello world :3

---
+++ src/main/java/net/jasminecrash/jasminestestmod/JasminesTestModClient.java
@@ -0,0 +1,10 @@
+package net.jasminecrash.jasminestestmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class JasminesTestModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
