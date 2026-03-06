838153df7efcea7e7946fa5e0d93c92ec47d6fb9
JasmineCrash <jazzminecrash@gmail.com>
2026-03-06 02:21:48 -0800

    quickly added a funny log message :3

--- src/main/java/net/jasminecrash/jasminestestmod/JasminesTestModClient.java
+++ src/main/java/net/jasminecrash/jasminestestmod/JasminesTestModClient.java
@@ -2,9 +2,24 @@ package net.jasminecrash.jasminestestmod;
 
 import net.fabricmc.api.ClientModInitializer;
 
+import java.util.Random;
+
 public class JasminesTestModClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
+        logRandomMessage();
+    }
 
+    private void logRandomMessage() {
+        String[] messages = {
+            "Hello from the depths of heck!",
+            "Client loaded successfully, and successfully loaded client!",
+            "Be gay, do crime!",
+            "Be crime, do gay~",
+            "how many redstoners does it take to- IS THAT HATSUNE MIKU???!!"
+        };
+        Random random = new Random();
+        String message = messages[random.nextInt(messages.length)];
+        JasminesTestMod.LOGGER.info(message);
     }
 }
